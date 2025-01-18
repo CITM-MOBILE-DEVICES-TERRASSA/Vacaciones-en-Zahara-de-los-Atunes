de7e097a693734eeee5620dee1b46828fc422d67
didacpema <didacpema@gmail.com>
2025-01-18 17:17:14 +0100

    CuttableParts & collision fix

--- Assets/Zahara-Deluxe/Scripts/Cuttable.cs
+++ Assets/Zahara-Deluxe/Scripts/Cuttable.cs
@@ -8,14 +8,17 @@ public class Cuttable : MonoBehaviour
     public void Cut()
     {
         // Instancia las dos mitades
-        GameObject topHalf = Instantiate(topHalfPrefab, new Vector3(transform.position.x+0.1f,transform.position.y+0.5f,transform.position.z), transform.rotation);
-        GameObject bottomHalf = Instantiate(bottomHalfPrefab, new Vector3(transform.position.x-0.1f,transform.position.y,transform.position.z), transform.rotation);
+        GameObject topHalf = Instantiate(topHalfPrefab, new Vector3(transform.position.x,transform.position.y+0.1f,transform.position.z), transform.rotation);
+        GameObject bottomHalf = Instantiate(bottomHalfPrefab, new Vector3(transform.position.x-0.2f,transform.position.y+0.1f,transform.position.z), transform.rotation);
 
         // Agrega físicas a las mitades
         Rigidbody topRb = topHalf.GetComponent<Rigidbody>();
         Rigidbody bottomRb = bottomHalf.GetComponent<Rigidbody>();
 
-        if (topRb != null) topRb.AddForce(Vector3.up * 2f, ForceMode.Impulse);
+        //if (topRb != null) topRb.AddForce(Vector3.up * 1f, ForceMode.Impulse);
+        //if (bottomRb != null) bottomRb.AddForce(Vector3.up * 1f, ForceMode.Impulse);
+
+        if (topRb != null) topRb.AddForce(Vector3.right * 5f, ForceMode.Impulse);
         if (bottomRb != null) bottomRb.AddForce(Vector3.left * 5f, ForceMode.Impulse);
 
         // Destruye el objeto original
