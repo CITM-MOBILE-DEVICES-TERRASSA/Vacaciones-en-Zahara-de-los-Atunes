aabd6a8a269ef545a80472b263720ddf985404a7
PauloWhite2004 <pau.blanco.valle@estudiantat.upc.edu>
2025-01-20 17:00:28 +0100

    cuttable como antes

--- Assets/Zahara-Deluxe/Scripts/Level1/Cuttable.cs
+++ Assets/Zahara-Deluxe/Scripts/Level1/Cuttable.cs
@@ -2,24 +2,23 @@ using UnityEngine;
 
 public class Cuttable : MonoBehaviour
 {
-    public GameObject topHalfPrefab;
-    public GameObject bottomHalfPrefab;
+    public GameObject topHalfPrefab; // Mitad superior del objeto
+    public GameObject bottomHalfPrefab; // Mitad inferior del objeto
     private GameObject topHalf;
     private GameObject bottomHalf;
-
     public void Cut()
     {
         // Instancia las dos mitades
-        topHalf = Instantiate(topHalfPrefab, new Vector3(transform.position.x, transform.position.y + 0.1f, transform.position.z), transform.rotation);
-        bottomHalf = Instantiate(bottomHalfPrefab, new Vector3(transform.position.x - 0.2f, transform.position.y + 0.1f, transform.position.z), transform.rotation);
-
-        // Desactivar el tag "Cuttable" en las mitades para que no puedan ser cortadas
-        topHalf.tag = "Untagged";
-        bottomHalf.tag = "Untagged";
+        topHalf = Instantiate(topHalfPrefab, new Vector3(transform.position.x,transform.position.y+0.1f,transform.position.z), transform.rotation);
+        bottomHalf = Instantiate(bottomHalfPrefab, new Vector3(transform.position.x-0.2f,transform.position.y+0.1f,transform.position.z), transform.rotation);
 
         // Agrega físicas a las mitades
         Rigidbody topRb = topHalf.GetComponent<Rigidbody>();
         Rigidbody bottomRb = bottomHalf.GetComponent<Rigidbody>();
+
+        //if (topRb != null) topRb.AddForce(Vector3.up * 1f, ForceMode.Impulse);
+        //if (bottomRb != null) bottomRb.AddForce(Vector3.up * 1f, ForceMode.Impulse);
+
         if (topRb != null) topRb.AddForce(Vector3.right * 5f, ForceMode.Impulse);
         if (bottomRb != null) bottomRb.AddForce(Vector3.left * 5f, ForceMode.Impulse);
 
@@ -27,20 +26,15 @@ public class Cuttable : MonoBehaviour
         Destroy(gameObject);
     }
 
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.C))
-        {
+    public void Update(){
+        if(Input.GetKeyDown(KeyCode.C)){
             Cut();
         }
-        if (topHalf != null && bottomHalf != null)
-        {
-            if (topHalf.transform.position.y < -10)
-            {
+        if(topHalf != null && bottomHalf != null){
+            if(topHalf.transform.position.y < -10){
                 Destroy(topHalf);
             }
-            if (bottomHalf.transform.position.y < -10)
-            {
+            if(bottomHalf.transform.position.y < -10){
                 Destroy(bottomHalf);
             }
         }
